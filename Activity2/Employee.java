1454d8b1fc5ad3305ef8fecb3e5a88a1636160dc
GaneshReddy99 <78849231+GaneshReddy99@users.noreply.github.com>
2021-02-27 09:24:40 +0530

    Add files via upload

---
+++ Activity2/Employee.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Employee
+{
+private String name;
+private String address;
+private String mobile;
+public String getname()
+{
+return name;
+}
+public String getaddress()
+{
+return address;
+}
+public String getmobile()
+{
+return mobile;
+}
+void setname(String nam)
+{
+this.name=nam;
+}
+void setaddress(String add)
+{
+this.address=add;
+}
+void setmobile(String mob)
+{
+this.mobile=mob;
+}
+}
